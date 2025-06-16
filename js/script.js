181d00a6f38afe52b2cb60cd4af1b17fbaa81d46
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-06-16 17:24:48 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 // https://hollandsweb.com/javascript-tutorial-1-copy-the-text-to-clipboard-using-javascript
 
 // Get references to all the copy buttons and code elements
-const copyButtons = document.querySelectorAll(".copy-icon");
-const elementsToCopy = document.querySelectorAll("code");
+let tooltipItems = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const elementsToCopy = document.querySelectorAll("code"); 
+
+// Initialize Bootstrap Tooltips
+let tooltipTriggerList = [].slice.call(tooltipItems);
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+})
 
 // Add click event listeners to all the copy buttons
-copyButtons.forEach((copyButton, index) => {
-  copyButton.addEventListener("click", () => {
+tooltipItems.forEach((tooltipItem, index) => {
+  tooltipItem.addEventListener("click", (e) => {
     // Get the corresponding element’s text content
-    const textToCopy = elementsToCopy[index].textContent;
+    const textToCopy = elementsToCopy[index].textContent; 
     // Create a temporary textarea element to copy the text to the clipboard
     const textarea = document.createElement("textarea");
-    textarea.value = textToCopy;
+    textarea.value = textToCopy; 
     // Append the textarea to the document
-    document.body.appendChild(textarea);
+    document.body.appendChild(textarea); 
     // Select the text within the textarea
-    textarea.select();
+    textarea.select(); 
     // Copy the selected text to the clipboard
-    document.execCommand("copy");
+    document.execCommand("copy"); 
     // Remove the temporary textarea
-    document.body.removeChild(textarea);
-    // Provide some visual feedback to the user (e.g., an alert)
-    alert("Text has been copied to the clipboard: " + textToCopy);
+    document.body.removeChild(textarea); 
+    // Provide some visual feedback to the user
+    let tooltip = e.target;
+    // https://github.com/twbs/bootstrap/issues/36432
+    // https://stackoverflow.com/questions/65862446/bootstrap-5-update-tooltip-title-with-javascript/65863062#65863062
+    const tooltipInstance = bootstrap.Tooltip.getInstance(tooltip);
+    tooltipInstance.setContent({ '.tooltip-inner': 'Copied!'});
+    setTimeout(() => {
+      tooltipInstance.setContent({ '.tooltip-inner': 'Copy'});
+    }, 1500);
   });
 });
