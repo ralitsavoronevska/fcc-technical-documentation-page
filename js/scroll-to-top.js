51d82fd36d6a9e8e87041e356257ebf6eba2a5ab
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-07-19 16:56:17 +0300

    Updates

--- js/scroll-to-top.js
+++ js/scroll-to-top.js
@@ -1,14 +1,7 @@
-(function (){
-  "use strict";
-  let scroll = document.querySelector('.scrollTop');
+window.addEventListener("DOMContentLoaded", () => {
+  let scroll = document.querySelector('.scrollToTop');
   window.addEventListener('scroll', function() {
     scroll.classList.toggle("active", window.scrollY > 500);
   });
   scroll.onclick = function() { window.scrollTo({ top: 0, behavior: 'smooth' }); }
-}());
-
-AOS.init({
-  offset: 400, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000 // values from 0 to 3000, with step 50ms
 });
